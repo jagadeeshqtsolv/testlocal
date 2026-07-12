f9277e91f9001a62d92ca70a778cbd0fca9fbb03
jaga <jagadeesh@qtsolv.com>
2026-07-13 02:52:07 +0530

    chore: sync test framework [2026-07-12T21:22:07.172Z]

--- support/fixtures.ts
+++ support/fixtures.ts
@@ -1,23 +1,18 @@
 import { test as base, expect } from "@playwright/test";
-import { HomePage } from "../pageobjects/HomePage";
-import { ProfilePage } from "../pageobjects/ProfilePage";
 import { SavedNewsPage } from "../pageobjects/SavedNewsPage";
+import { UkgcommonsPage } from "../pageobjects/UkgcommonsPage";
 
 type AppFixtures = {
-  homePage: HomePage;
-  profilePage: ProfilePage;
   savedNewsPage: SavedNewsPage;
+  ukgcommonsPage: UkgcommonsPage;
 };
 
 export const test = base.extend<AppFixtures>({
-  homePage: async ({ page }, use) => {
-    await use(new HomePage(page));
-  },
-  profilePage: async ({ page }, use) => {
-    await use(new ProfilePage(page));
-  },
   savedNewsPage: async ({ page }, use) => {
     await use(new SavedNewsPage(page));
+  },
+  ukgcommonsPage: async ({ page }, use) => {
+    await use(new UkgcommonsPage(page));
   },
 });
 
